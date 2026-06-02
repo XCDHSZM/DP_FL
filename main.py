faac702875458e5a0c062b1c974f44e72b3c79c8
申智铭 <11059016+zhiming3150@user.noreply.gitee.com>
2026-06-02 20:16:30 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from Fed import fed_train
+from GRNN import grnn_lenet_attack
+
+if __name__ == '__main__':
+    command_str = "--dataset mnist --model LeNet --dp_mechanism Gaussian --dp_epsilon 15 --dp_clip 10 --lr_decay 1 --dp_composition Simple"
+    fed_train(command_str)
+    grnn_lenet_attack(command_str)
